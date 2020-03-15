6325a3fdfc14c50ad4af918602f53ad5787ae84e
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2020-03-15 11:58:32 +0800

    Topic master: ◉ ◉ ◉
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

---
+++ topic/master.go
@@ -0,0 +1,19 @@
+package topic
+
+import (
+	"github.com/jiangxin/git-merge-demo/common"
+)
+
+type Master struct{}
+
+func (v Master) Symbol() string {
+	return "◉"
+}
+
+func (v Master) Count() int {
+	return 3
+}
+
+func init() {
+	common.Register(&Master{})
+}
