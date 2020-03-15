8f199712a810787022fbb80a165645a3a466272a
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2020-03-15 10:10:55 +0800

    Topic 1: ♠ ♠ ♠
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

---
+++ topic/topic1.go
@@ -0,0 +1,19 @@
+package topic
+
+import (
+	"github.com/jiangxin/git-merge-demo/common"
+)
+
+type Topic_1 struct{}
+
+func (v Topic_1) Symbol() string {
+	return "♠"
+}
+
+func (v Topic_1) Count() int {
+	return 3
+}
+
+func init() {
+	common.Register(&Topic_1{})
+}
